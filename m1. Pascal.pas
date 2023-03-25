b6ad53a9bf469a56332cdb96fdd5e84bee59c5be
Ridho Aji <ridhoaji921@gmail.com>
2023-03-25 13:49:46 +0700

    Minggu 1

---
+++ m1. Pascal.pas
@@ -0,0 +1,29 @@
+program DataMhs;
+
+var nama, npm, kelas:array[1..3] of string;
+var i:integer;
+
+begin
+   for i := 1  to 3 do
+   begin
+  writeln();
+  writeln('Data ke-', i);
+    write ('Nama: ');
+    readln(nama[i]);
+    write ('NPM: ');
+    readln(NPM[i]);
+    write ('Kelas: ');
+    readln(kelas[i]);
+   end;
+   
+   writeln('======================');
+
+   for i := 1  to 3 do
+   begin
+  writeln();
+  writeln('Data ke-', i);
+  writeln ('Nama: ', nama[i]);
+  writeln ('NPM: ', NPM[i]);
+  writeln ('Kelas: ', kelas[i]);
+  end;
+end.
